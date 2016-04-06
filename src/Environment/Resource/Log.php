a7ad0617af20f2cd6dc26fe924df6d730131f451
Christian Würker <kriss@ceusmedia.de>
2016-04-06 04:53:18 +0200

    Extend basic logger by hook calls.

--- src/Environment/Resource/Log.php
+++ src/Environment/Resource/Log.php
@@ -1,17 +1,37 @@
 <?php
 class CMF_Hydrogen_Environment_Resource_Log {
-	
+
 	protected $env;
 	protected $path;
-	
+
 	public function __construct( $env ){
 		$this->env	= $env;
 		$this->path	= $env->getConfig()->get( 'path.logs' );
 	}
 
-	public function record( $category, $message, $file, $line ){
-		error_log( time().': '.$message."\n", 3, $this->path.$category.'.log' );
-		
+	/**
+	 *	@trigger		Env::log
+	 */
+	public function log( $type, $message, $file = NULL, $line = NULL ){
+		$data	= array(
+			'type'		=> $type,
+			'message'	=> $message,
+			'file'		=> $file,
+			'line'		=> $line,
+		);
+		if( $this->env->getCaptain()->callHook( 'Env', 'log', $this, $data ) )
+			return;
+		error_log( time().': '.$message."\n", 3, $this->path.$type.'.log' );
+	}
+
+	/**
+	 *	@trigger		Env::logException
+	 */
+	public function logException( $exception ){
+		$data	= array( 'exception' => $exception );
+		if( $this->env->getCaptain()->callHook( 'Env', 'logException', $this, $data ) )
+			return;
+		error_log( time().': '.$exception->getMessage()."\n", 3, $this->path.'exception.log' );
 	}
 }
 ?>
