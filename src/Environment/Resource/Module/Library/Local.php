f30aefd5a1d587bb30c64468599260ac36021223
Christian Würker <kriss@ceusmedia.de>
2018-01-26 04:30:38 +0100

    Fix bug.

--- src/Environment/Resource/Module/Library/Local.php
+++ src/Environment/Resource/Module/Library/Local.php
@@ -43,7 +43,8 @@ class CMF_Hydrogen_Environment_Resource_Module_Library_Local implements CMF_Hydr
 	public function __construct( CMF_Hydrogen_Environment_Abstract $env ){
 		$this->env		= $env;
 		$config			= $this->env->getConfig();
-		$this->path		= $this->env::$configPath.'modules/';
+		$envReflection	= new ReflectionObject( $this->env );
+		$this->path		= $envReflection->getConstant( 'configPath' ).'modules/';
 		if( $config->get( 'path.module.config' ) )
 			$this->path	= $config->get( 'path.module.config' );
 		$this->path		= $env->uri.$this->path;
