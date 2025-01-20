0a8143de437ff0f635923d20933486cd4f89344b
kriss0r <kriss@ceusmedia.de>
2025-01-20 13:21:40 +0100

    Extends entity to implement ArrayAccess.

--- src/Entity.php
+++ src/Entity.php
@@ -9,10 +9,20 @@
  */
 namespace CeusMedia\HydrogenFramework;
 
+use ArrayAccess;
 use CeusMedia\Common\ADT\Collection\Dictionary;
+use CeusMedia\Common\Exception\Data\Missing as MissingException;
+use CeusMedia\Common\Exception\Runtime;
+use ReflectionProperty;
 
-class Entity
+/**
+ * @implements ArrayAccess<string, mixed>
+ */
+class Entity implements ArrayAccess
 {
+	protected static array $mandatoryFields		= [];
+	protected static array $presetValues		= [];
+
 	/**
 	 *	@param		array		$data
 	 *	@return		static
@@ -38,16 +48,19 @@ class Entity
 	 */
 	public function __construct( Dictionary|array $data = [] )
 	{
-		if( $data instanceof Dictionary )
-			$data	= $data->getAll();
+		/** @var array $array */
+		$array	= ( $data instanceof Dictionary ) ? $data->getAll() : $data;
+		$array	= array_merge( self::$presetValues, $array );
+
+		self::checkMandatoryFields( $array );
+		self::checkValues( $array );
 
 		/**
 		 * @var string $key
 		 * @var string|int|float|NULL $value
 		 */
-		foreach( $data as $key => $value )
-			if( property_exists( $this, $key ) )
-				$this->set( $key, $value );
+		foreach( $array as $key => $value )
+			$this->set( $key, $value );
 	}
 
 	/**
@@ -56,9 +69,7 @@ class Entity
 	 */
 	public function get( string $key ): int|float|string|array|object|NULL
 	{
-		if( property_exists( $this, $key ) )
-			return $this->$key;
-		return NULL;
+		return self::isPublicProperty( $key ) ? $this->$key : NULL;
 	}
 
 	/**
@@ -67,7 +78,7 @@ class Entity
 	 */
 	public function has( string $key ): bool
 	{
-		return property_exists( $this, $key ) && NULL !== $this->$key;
+		return self::isPublicProperty( $key ) && NULL !== $this->$key;
 	}
 
 	/**
@@ -90,5 +101,50 @@ class Entity
 		return array_map( function( $value ){
 			return $value;
 		}, get_object_vars( $this ) );
+	}
+
+
+	//  --  PROTECTED  --  //
+
+
+	protected static function checkMandatoryFields( array $data ): void
+	{
+		foreach( self::$mandatoryFields as $key )
+			if( !array_key_exists( $key, $data ) )
+				throw MissingException::create( 'Missing data for key "'.$key.'"' );
+	}
+
+	protected static function checkValues( array $data ): void
+	{
+	}
+
+	protected static function isPublicProperty( string $key ): bool
+	{
+		if( !property_exists( static::class, $key ) )
+			return FALSE;
+		$reflection	= new ReflectionProperty( static::class, $key );
+		return $reflection->isPublic();
+	}
+
+	public function offsetExists( mixed $offset ): bool
+	{
+		return $this->has( $offset );
+	}
+
+	public function offsetGet( mixed $offset ): mixed
+	{
+		return $this->get( $offset );
+	}
+
+	public function offsetSet( mixed $offset, mixed $value ): void
+	{
+		if( NULL === $offset )
+			throw Runtime::create( 'Key must not be null' );
+		$this->set( $offset, $value );
+	}
+
+	public function offsetUnset( mixed $offset ): void
+	{
+		$this->set( $offset, NULL );
 	}
 }
