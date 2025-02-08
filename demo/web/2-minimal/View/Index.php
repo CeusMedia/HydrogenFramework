ad38207f0141740255acca6ad0c270bfe06f6c73
kriss0r <kriss@ceusmedia.de>
2025-02-08 13:57:12 +0100

    Support custom hooks and update demos.

--- demo/web/2-minimal/View/Index.php
+++ demo/web/2-minimal/View/Index.php
@@ -3,9 +3,15 @@ use CeusMedia\HydrogenFramework\View;
 
 class View_Index extends View
 {
-	public function index()
+	public function index(): string
 	{
 		extract( $this->getData() );
-		return 'View says: Hello World! The microtime is: '.$microtime;
+		$payload	= ['number' => 0];
+		$this->env->getCaptain()->callHookWithPayload( 'Custom', 'random', $this, $payload );
+
+		return join( '<br/>', [
+			'View says: Hello World! The microtime is: '.$microtime,
+			'Custom hook rolled random number '.$payload['number'],
+		] );
 	}
 }
