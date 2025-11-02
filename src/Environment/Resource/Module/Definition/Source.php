b2189fbc5666c15224c132b8e9e285fc61689ac6
kriss0r <kriss@ceusmedia.de>
2025-11-02 06:40:59 +0100

    Extend module definition by sources.

---
+++ src/Environment/Resource/Module/Definition/Source.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ *	Module definition: Source.
+ *
+ *	Copyright (c) 2025 Christian Würker (ceusmedia.de)
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *	@category		Library
+ *	@package		CeusMedia.HydrogenFramework.Environment.Resource.Module.Definition
+ *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
+ *	@copyright		2025 Christian Würker (ceusmedia.de)
+ *	@license		https://www.gnu.org/licenses/gpl-3.0.txt GPL 3
+ *	@link			https://github.com/CeusMedia/HydrogenFramework
+ */
+
+namespace CeusMedia\HydrogenFramework\Environment\Resource\Module\Definition;
+
+/**
+ *	Module definition: Source.
+ *
+ *	@category		Library
+ *	@package		CeusMedia.HydrogenFramework.Environment.Resource.Module.Definition
+ *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
+ *	@copyright		2025 Christian Würker (ceusmedia.de)
+ *	@license		https://www.gnu.org/licenses/gpl-3.0.txt GPL 3
+ *	@link			https://github.com/CeusMedia/HydrogenFramework
+ */
+class Source
+{
+	/** @var	string				$name */
+	public string $name;
+
+
+	/** @var	string|NULL			$url */
+	public ?string $url				= NULL;
+
+	/**
+	 *	@param		string			$name
+	 *	@param		string|NULL		$url
+	 */
+	public function __construct( string $name, ?string $url )
+	{
+		$this->name		= $name;
+		$this->url		= $url;
+	}
+}
