206e698b7c86c1342ef6353a5348ef62acbe8088
kriss0r <kriss@ceusmedia.de>
2026-03-18 04:21:01 +0100

    Update copyright year and set version 1.0.2.

--- src/Model/Database/Table.php
+++ src/Model/Database/Table.php
@@ -5,7 +5,7 @@
  *	Generic Model Class of Framework Hydrogen.
  * 	Acts as a binding to PDO table implementation in CeusMedia::Database.
  *
- *	Copyright (c) 2007-2025 Christian Würker (ceusmedia.de)
+ *	Copyright (c) 2007-2026 Christian Würker (ceusmedia.de)
  *
  *	This program is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@
  *	@category		Library
  *	@package		CeusMedia.HydrogenFramework
  *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
- *	@copyright		2007-2025 Christian Würker (ceusmedia.de)
+ *	@copyright		2007-2026 Christian Würker (ceusmedia.de)
  *	@license		https://www.gnu.org/licenses/gpl-3.0.txt GPL 3
  *	@link			https://github.com/CeusMedia/HydrogenFramework
  */
@@ -43,7 +43,7 @@ use RuntimeException;
  *	@category		Library
  *	@package		CeusMedia.HydrogenFramework
  *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
- *	@copyright		2007-2025 Christian Würker (ceusmedia.de)
+ *	@copyright		2007-2026 Christian Würker (ceusmedia.de)
  *	@license		https://www.gnu.org/licenses/gpl-3.0.txt GPL 3
  *	@link			https://github.com/CeusMedia/HydrogenFramework
  */
